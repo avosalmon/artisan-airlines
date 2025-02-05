28190076daec41dbb3d1fa5851aa11371fba795f
Ryuta Hamasaki <avosalmon@gmail.com>
2025-02-05 16:40:36 +0900

    add checkin module

---
+++ app-modules/checkin/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+// use Modules\Checkin\Http\Controllers\CheckinController;
+
+// Route::get('/checkins', [CheckinController::class, 'index'])->name('checkins.index');
+// Route::get('/checkins/create', [CheckinController::class, 'create'])->name('checkins.create');
+// Route::post('/checkins', [CheckinController::class, 'store'])->name('checkins.store');
+// Route::get('/checkins/{checkin}', [CheckinController::class, 'show'])->name('checkins.show');
+// Route::get('/checkins/{checkin}/edit', [CheckinController::class, 'edit'])->name('checkins.edit');
+// Route::put('/checkins/{checkin}', [CheckinController::class, 'update'])->name('checkins.update');
+// Route::delete('/checkins/{checkin}', [CheckinController::class, 'destroy'])->name('checkins.destroy');
---
+++ app-modules/checkin/src/Providers/CheckinServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Checkin\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CheckinServiceProvider extends ServiceProvider
+{
+    public function register(): void {}
+
+    public function boot(): void {}
+}
