220bbbd05254ba564d108ca205e4601bac09a992
Ryuta Hamasaki <avosalmon@gmail.com>
2025-02-05 16:47:03 +0900

    add loyalty module

---
+++ app-modules/loyalty/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+// use Modules\Loyalty\Http\Controllers\LoyaltyController;
+
+// Route::get('/loyalties', [LoyaltyController::class, 'index'])->name('loyalties.index');
+// Route::get('/loyalties/create', [LoyaltyController::class, 'create'])->name('loyalties.create');
+// Route::post('/loyalties', [LoyaltyController::class, 'store'])->name('loyalties.store');
+// Route::get('/loyalties/{loyalty}', [LoyaltyController::class, 'show'])->name('loyalties.show');
+// Route::get('/loyalties/{loyalty}/edit', [LoyaltyController::class, 'edit'])->name('loyalties.edit');
+// Route::put('/loyalties/{loyalty}', [LoyaltyController::class, 'update'])->name('loyalties.update');
+// Route::delete('/loyalties/{loyalty}', [LoyaltyController::class, 'destroy'])->name('loyalties.destroy');
---
+++ app-modules/loyalty/src/Providers/LoyaltyServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Loyalty\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LoyaltyServiceProvider extends ServiceProvider
+{
+	public function register(): void
+	{
+	}
+	
+	public function boot(): void
+	{
+	}
+}
