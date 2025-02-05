f70c40e653c32b72cc40b3810494fa0867828690
Ryuta Hamasaki <avosalmon@gmail.com>
2025-02-05 16:38:14 +0900

    add booking module

---
+++ app-modules/booking/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+// use Modules\Booking\Http\Controllers\BookingController;
+
+// Route::get('/bookings', [BookingController::class, 'index'])->name('bookings.index');
+// Route::get('/bookings/create', [BookingController::class, 'create'])->name('bookings.create');
+// Route::post('/bookings', [BookingController::class, 'store'])->name('bookings.store');
+// Route::get('/bookings/{booking}', [BookingController::class, 'show'])->name('bookings.show');
+// Route::get('/bookings/{booking}/edit', [BookingController::class, 'edit'])->name('bookings.edit');
+// Route::put('/bookings/{booking}', [BookingController::class, 'update'])->name('bookings.update');
+// Route::delete('/bookings/{booking}', [BookingController::class, 'destroy'])->name('bookings.destroy');
---
+++ app-modules/booking/src/Providers/BookingServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Booking\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class BookingServiceProvider extends ServiceProvider
+{
+    public function register(): void {}
+
+    public function boot(): void {}
+}
