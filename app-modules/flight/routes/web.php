1829f12c88fc92dd8fc56df59765b5a70894bad1
Ryuta Hamasaki <avosalmon@gmail.com>
2025-02-05 15:53:54 +0900

    add flight module

---
+++ app-modules/flight/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+// use Modules\Flight\Http\Controllers\FlightController;
+
+// Route::get('/flights', [FlightController::class, 'index'])->name('flights.index');
+// Route::get('/flights/create', [FlightController::class, 'create'])->name('flights.create');
+// Route::post('/flights', [FlightController::class, 'store'])->name('flights.store');
+// Route::get('/flights/{flight}', [FlightController::class, 'show'])->name('flights.show');
+// Route::get('/flights/{flight}/edit', [FlightController::class, 'edit'])->name('flights.edit');
+// Route::put('/flights/{flight}', [FlightController::class, 'update'])->name('flights.update');
+// Route::delete('/flights/{flight}', [FlightController::class, 'destroy'])->name('flights.destroy');
---
+++ app-modules/flight/src/Providers/FlightServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Flight\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class FlightServiceProvider extends ServiceProvider
+{
+    public function register(): void {}
+
+    public function boot(): void {}
+}
