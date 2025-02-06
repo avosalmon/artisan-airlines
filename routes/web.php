c5e0480cc82172442e57443ede95f57d52c64e54
Ryuta Hamasaki <avosalmon@gmail.com>
2025-02-06 16:11:34 +0900

    Remove dashboard

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,6 @@ use App\Http\Controllers\Settings\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('dashboard', function () {
-    return Inertia::render('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::redirect('settings', 'settings/profile');
 
