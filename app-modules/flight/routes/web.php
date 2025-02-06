66b3387a5b286e0a69a83c173a75105d4363dd9f
Ryuta Hamasaki <avosalmon@gmail.com>
2025-02-06 17:22:01 +0900

    Add search route

--- app-modules/flight/routes/web.php
+++ app-modules/flight/routes/web.php
@@ -4,5 +4,7 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 use Modules\Flight\Http\Controllers\FlightController;
+use Modules\Flight\Http\Controllers\SearchController;
 
 Route::get('/', [FlightController::class, 'index']);
+Route::get('/search', [SearchController::class, 'index']);
