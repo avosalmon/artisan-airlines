cb21a2f909c90c4582bb8a994735f1342856b534
Ryuta Hamasaki <avosalmon@gmail.com>
2025-02-07 23:52:01 +0900

    Apply web middleware

--- app-modules/booking/routes/web.php
+++ app-modules/booking/routes/web.php
@@ -6,5 +6,7 @@ use Illuminate\Support\Facades\Route;
 use Modules\Booking\Http\Controllers\BookingController;
 use Modules\Booking\Http\Controllers\PendingBookingController;
 
-Route::post('/bookings/pending', [PendingBookingController::class, 'store'])->name('booking.pending.store');
-Route::get('/bookings/{booking}/passengers', [BookingController::class, 'create'])->name('booking.passengers.create');
+Route::middleware('web')->group(function () {
+    Route::post('/bookings/pending', [PendingBookingController::class, 'store'])->name('booking.pending.store');
+    Route::get('/bookings/{booking}/passengers', [BookingController::class, 'create'])->name('booking.passengers.create');
+});
--- app-modules/flight/routes/web.php
+++ app-modules/flight/routes/web.php
@@ -6,5 +6,7 @@ use Illuminate\Support\Facades\Route;
 use Modules\Flight\Http\Controllers\FlightController;
 use Modules\Flight\Http\Controllers\SearchController;
 
-Route::get('/', [FlightController::class, 'index']);
-Route::get('/flights/search', [SearchController::class, 'index']);
+Route::middleware('web')->group(function () {
+    Route::get('/', [FlightController::class, 'index']);
+    Route::get('/flights/search', [SearchController::class, 'index']);
+});
