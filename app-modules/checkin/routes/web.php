bc96fb6fc193be7af3990e75f066baa3b6e20242
Ryuta Hamasaki <avosalmon@gmail.com>
2025-02-18 21:27:18 +0900

    Add unit and feature test folders

--- app-modules/checkin/routes/web.php
+++ app-modules/checkin/routes/web.php
@@ -2,12 +2,8 @@
 
 declare(strict_types=1);
 
-// use Modules\Checkin\Http\Controllers\CheckinController;
+use Illuminate\Support\Facades\Route;
 
-// Route::get('/checkins', [CheckinController::class, 'index'])->name('checkins.index');
-// Route::get('/checkins/create', [CheckinController::class, 'create'])->name('checkins.create');
-// Route::post('/checkins', [CheckinController::class, 'store'])->name('checkins.store');
-// Route::get('/checkins/{checkin}', [CheckinController::class, 'show'])->name('checkins.show');
-// Route::get('/checkins/{checkin}/edit', [CheckinController::class, 'edit'])->name('checkins.edit');
-// Route::put('/checkins/{checkin}', [CheckinController::class, 'update'])->name('checkins.update');
-// Route::delete('/checkins/{checkin}', [CheckinController::class, 'destroy'])->name('checkins.destroy');
+Route::middleware('web')->group(function () {
+    //
+});
