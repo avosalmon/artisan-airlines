a1883ea8670957da5911f4b01441708bb50918e2
Ryuta Hamasaki <avosalmon@gmail.com>
2025-02-18 20:55:57 +0900

    wip

--- app-modules/booking/resources/js/pages/show.tsx
+++ app-modules/booking/resources/js/pages/show.tsx
@@ -1,12 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import FormLayout from "@/layouts/form-layout";
-import { getDuration } from "@/lib/utils";
 import { PageProps } from "@/types";
 import { Booking } from "@booking/index";
 import { Flight } from "@flight/index";
 import { Head, Link } from "@inertiajs/react";
-import { CheckCircle, Plane } from "lucide-react";
+import { format } from "date-fns";
+import { CheckCircle } from "lucide-react";
 
 export default function Show({ booking, flight }: PageProps<{ booking: Booking; flight: Flight }>) {
   return (
@@ -42,23 +42,18 @@ export default function Show({ booking, flight }: PageProps<{ booking: Booking; 
                 <p className="text-lg font-semibold">
                   {flight.origin_airport?.city} ({flight.origin_airport?.iata_code})
                 </p>
-                <p className="text-sm text-muted-foreground">{new Date(flight.departure_time).toLocaleString()}</p>
+                <p className="text-muted-foreground">{format(new Date(flight.departure_time), "dd MMM (EEE) HH:mm")}</p>
               </div>
-              <Plane className="h-8 w-8 justify-self-center" />
               <div>
                 <p className="text-sm font-medium text-muted-foreground">To</p>
                 <p className="text-lg font-semibold">
                   {flight.destination_airport?.city} ({flight.destination_airport?.iata_code})
                 </p>
-                <p className="text-sm text-muted-foreground">{new Date(flight.arrival_time).toLocaleString()}</p>
+                <p className="text-muted-foreground">{format(new Date(flight.arrival_time), "dd MMM (EEE) HH:mm")}</p>
               </div>
             </div>
 
-            <div className="grid grid-cols-3 gap-4">
-              <div>
-                <p className="text-sm font-medium text-muted-foreground">Duration</p>
-                <p className="text-lg font-semibold">{getDuration(flight.departure_time, flight.arrival_time)}</p>
-              </div>
+            <div className="grid grid-cols-2 gap-4">
               <div>
                 <p className="text-sm font-medium text-muted-foreground">Passengers</p>
                 <p className="text-lg font-semibold">{booking.passengers?.length}</p>
